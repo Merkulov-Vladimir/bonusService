dcece601d6e27fc2e79ddfc9a9c8d0a036693007
Vladimir Merkulov <merkulov-vladimir93@mail.ru>
2022-02-09 05:02:37 +0700

    fixed #1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,8 +4,12 @@ public class Main {
 
     long amount = 1000_60;
     boolean registered = true;
-
-    service.calculate(amount, registered);
+    long expected = 30;
+    // вызываем целевой метод
+    long actual = service.calculate(amount, registered);
+    // производим проверку (сравниваем ожидаемый и фактический)
+    boolean passed = expected == actual;
+    System.out.println(passed);
   }
 }
 
